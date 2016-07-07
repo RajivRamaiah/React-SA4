6126cccdd9dee778c31617d37e3d46b45063ab70
Rajiv Ramaiah <rajivramaiah@me.com>
2016-07-07 11:28:50 -0400

    added all necessary libraries

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let i = 1;
+
+$('#main').html('You have been on this page for ${i} seconds!');
+
+// var i = 1;
+// setInterval( increment, 1000);
+//
+// function increment(){
+//     i = i % 360 + 1;
+//     $('#main').html('You have been on this page for ${i} seconds!');
+// }
+
+// var myVar = setInterval(myTimer, 1000);
+//
+// function myTimer() {
+//     var i = 1
+//     document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+//     $('#main').html('You have been on this page for ${i} seconds!');
+// }
