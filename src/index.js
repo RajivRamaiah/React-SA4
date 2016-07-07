50311dd391305fd8581bf1ad9f964d58f3569256
Rajiv Ramaiah <rajivramaiah@me.com>
2016-07-07 14:03:27 -0400

    fixed a small error

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import $ from 'jquery';
 import './style.scss';
 
 // The following code was inspired by http://stackoverflow.com/questions/10586890/increment-integer-by-1-every-1-second
-let i = 1;
+let num = 1;
 setInterval(increment, 1000);
 
 function increment(){
-    i = i % 360 + 1;
-    $('#main').html(`You have been on this page for ${i} seconds!`);
+    num = num % 360 + 1;
+    $('#main').html(`You have been on this page for ${num} seconds!`);
 }
