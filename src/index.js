8adb7578e9f1c813805f80a7f8a75e39fdd3c3bc
Rajiv Ramaiah <rajivramaiah@me.com>
2016-07-07 11:45:52 -0400

    seconds up and running

--- src/index.js
+++ src/index.js
@@ -2,22 +2,11 @@
 import $ from 'jquery';
 import './style.scss';
 
-let i = 1;
+var i = 1;
 
-$('#main').html('You have been on this page for ${i} seconds!');
+var interval = setInterval( increment, 1000);
 
-// var i = 1;
-// setInterval( increment, 1000);
-//
-// function increment(){
-//     i = i % 360 + 1;
-//     $('#main').html('You have been on this page for ${i} seconds!');
-// }
-
-// var myVar = setInterval(myTimer, 1000);
-//
-// function myTimer() {
-//     var i = 1
-//     document.getElementById("demo").innerHTML = d.toLocaleTimeString();
-//     $('#main').html('You have been on this page for ${i} seconds!');
-// }
+function increment(){
+    i = i % 360 + 1;
+    $('#main').html(`You have been on this page for ${i} seconds!`);
+}
