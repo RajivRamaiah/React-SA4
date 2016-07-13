d29c827853f8d2dfcfe31a71d6be6ac15fc31eb8
Rajiv Ramaiah <rajivramaiah@me.com>
2016-07-13 16:46:37 -0400

    finished up to video list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 // change require to es6 import style
 import $ from 'jquery';
 import './style.scss';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import youtubeSearch from './youtube-api.js';
+import React, { Component } from 'react';
 
-// The following code was inspired by http://stackoverflow.com/questions/10586890/increment-integer-by-1-every-1-second
-let num = 1;
-setInterval(increment, 1000);
 
-function increment(){
-    num = num % 360 + 1;
-    $('#main').html(`You have been on this page for ${num} seconds!`);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    youtubeSearch('pixar').then(videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
+// AIzaSyBaLEMEffxGezoLNKBfBGJoHg37iXSmP8o
+
+// // The following code was inspired by http://stackoverflow.com/questions/10586890/increment-integer-by-1-every-1-second
+// const App = () => {
+//   return (
+//     <div>
+//       <SearchBar />
+//     </div>
+//   );
+// };
+//
+ReactDOM.render(<App />, document.getElementById('main'));
