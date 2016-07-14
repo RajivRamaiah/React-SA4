c2ba92d1be76d48b0270eafdb189b12a4332808e
Rajiv Ramaiah <rajivramaiah@me.com>
2016-07-13 21:55:41 -0400

    completed SA4

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import debounce from 'lodash.debounce';
 
 class SearchBar extends Component {
 
@@ -8,11 +9,14 @@ class SearchBar extends Component {
     this.state = { searchterm: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
+
+    // this.search = debounce(this.search, 300);
   }
 
   onInputChange(event) {
     console.log(event.target.value);
     this.setState({ searchterm: event.target.value });
+    this.props.onSearchChange(this.state.searchterm);
   }
 
   render() {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-// change require to es6 import style
-import $ from 'jquery';
 import './style.scss';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import youtubeSearch from './youtube-api.js';
 import React, { Component } from 'react';
+import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_detail';
 
 
 class App extends Component {
@@ -16,7 +16,11 @@ class App extends Component {
       selectedVideo: null,
     };
 
-    youtubeSearch('pixar').then(videos => {
+    this.search('pixar');
+  }
+
+  search(text) {
+    youtubeSearch(text).then(videos => {
       this.setState({
         videos,
         selectedVideo: videos[0],
@@ -24,23 +28,24 @@ class App extends Component {
     });
   }
 
+
   render() {
+    if (!this.state.selectedVideo) {
+      return <div>Loading...</div>;
+    }
     return (
       <div>
-        <SearchBar />
+        <div id="video-section">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos} />
+        </div>
+
+        <div id="searchbar">
+          <SearchBar onSearchChange={text => this.search(text)} />
+        </div>
       </div>
     );
   }
 }
-// AIzaSyBaLEMEffxGezoLNKBfBGJoHg37iXSmP8o
 
-// // The following code was inspired by http://stackoverflow.com/questions/10586890/increment-integer-by-1-every-1-second
-// const App = () => {
-//   return (
-//     <div>
-//       <SearchBar />
-//     </div>
-//   );
-// };
-//
 ReactDOM.render(<App />, document.getElementById('main'));
